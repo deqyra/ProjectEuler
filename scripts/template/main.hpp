373ff2ed5a635e2862c0f0909bb298068ad48284
François BRACHAIS <francois.brachais@stormshield.eu>
2021-07-08 18:04:40 +0200

    add docstring

--- scripts/template/main.hpp
+++ scripts/template/main.hpp
@@ -3,6 +3,11 @@
 
 static constexpr unsigned int ProblemNumber = XXXX;
 
+/// @brief Solves problem XXXX of Project Euler.
+///
+/// @see https://projecteuler.net/problem=XXXX
+///
+/// @return The solution for problem XXXX of Project Euler.
 long long int solve();
 
 #endif//PROJECTEULER__SOLUTIONS__XXXX__MAIN_HPP
--- solutions/0001/main.hpp
+++ solutions/0001/main.hpp
@@ -3,6 +3,11 @@
 
 static constexpr unsigned int ProblemNumber = 0001;
 
+/// @brief Solves problem 0001 of Project Euler.
+///
+/// @see https://projecteuler.net/problem=0001
+///
+/// @return The solution for problem 0001 of Project Euler.
 long long int solve();
 
 #endif//PROJECTEULER__SOLUTIONS__0001__MAIN_HPP
